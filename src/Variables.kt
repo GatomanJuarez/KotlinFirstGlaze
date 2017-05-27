4fadc7e665b3d094c997c4faa77796abf6e4cb07
GatomanJuarez <juarez_devora_ja@hotmail.com>
2017-05-27 11:59:08 -0500

    Variables

---
+++ src/Variables.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by GatomanJuarez on 27/05/2017.
+ */
+
+fun main(args : Array<String>){
+    /** val = final */
+    val nombre = "JesúsJuarez"
+    /** se pueden cambiar su valor. */
+    var edad = 18
+    var edad2 : Int = 20
+}
