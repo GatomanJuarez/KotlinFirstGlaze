fb51a360934d75b44b1e02f9276e20c77b724fdb
GatomanJuarez <juarez_devora_ja@hotmail.com>
2017-05-27 12:00:15 -0500

    Pedir datos por consola

---
+++ src/PedirDatos.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+/**
+ * Created by GatomanJuarez on 27/05/2017.
+ */
+
+fun main (args: Array<String>){
+    println("¿Como te llamas?")
+    val scanner = Scanner(System.`in`)
+    val nextLine = scanner.nextLine()
+    val name = if(nextLine != "") nextLine else "Kotlin"
+
+    println("Hola $name")
+    println("Hola ${if(nextLine != "") nextLine else "Kotlin"}")
+}
